e1d99359d4596b3343b77ecfc22a910090b13650
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-07-07 00:36:35 +0430

    mobile responsive: In mobile size menu bars and its functionality is done!

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,3 +1,15 @@
 function toggleDarkMode() {
-    document.documentElement.classList.toggle('dark');
-  }
+  document.documentElement.classList.toggle('dark');
+}
+let remain
+
+function DropDownMode() {
+  document.querySelector('#toggle').addEventListener('click', () => {
+    let toggle_mode = document.querySelector('#dropdown');
+    if (toggle_mode.style.display == 'none') {
+      toggle_mode.style.display = 'block';
+    } else {
+      toggle_mode.style.display = 'none';
+    }
+  })
+}
