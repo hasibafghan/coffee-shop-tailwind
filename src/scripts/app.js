1cfc4db1f30aa7d39384ffb11f621dad745acdcc
Hasib Alamyar <hasibalamyar2@gmail.com>
2025-07-09 06:20:13 +0430

    Overlay background : blur the background when user open menu in mobile size

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -2,37 +2,72 @@ function toggleDarkMode() {
   document.documentElement.classList.toggle('dark');
 }
 
-function DropDownMode() {
-  document.querySelector('#toggle').addEventListener('click', () => {
-    let toggle_mode = document.querySelector('#dropdown');
-    if (toggle_mode.style.display == 'none') {
-      toggle_mode.style.display = 'block';
-    } else {
-      toggle_mode.style.display = 'none';
-    }
-  })
-}
 
 
-  // Wait for the DOM to load
-  document.addEventListener("DOMContentLoaded", function () {
-    const menuButton = document.getElementById("menu-toggle"); // ID for your bars-3 icon
-    const blurOverlay = document.getElementById("blur");
-    const close_offcanvas = document.getElementById("x-mark")
-    
-    if (!menuButton || !blurOverlay) return;
 
-    menuButton.addEventListener("click", function () {
-      blurOverlay.classList.toggle("hidden"); // Show/Hide the overlay
-    });
+// Wait for the DOM to load
+document.addEventListener("DOMContentLoaded", function () {
+  // right menu section IDs
+  const dropdown_of_shop = document.querySelector('#dropdown');
+  const toggle_for_dropdown = document.querySelector('#toggle')
+  const menu_button_right_bars_3 = document.querySelector("#menu-button-right-bars-3");
+  const offcanvas_of_right_menu = document.querySelector("#offcanvas-of-right-menu");
+  const close_btn_of_right_menu_inside = document.querySelector("#close-btn-of-right-menu-inside");
+  const blurOverlay = document.querySelector("#blur");
+  // left menu section IDs 
+  const menu_button_left_shopping_cart = document.querySelector("#menu-button-left-shopping-cart");
+  const offcanvas_of_left_menu = document.querySelector("#offcanvas-of-left-menu");
+  const close_btn_of_left_menu_inside = document.querySelector("#close-btn-of-left-menu-inside");
+  
+  // -----------------------------------------------
 
-    // Optional: Click outside the menu to remove the blur
-    blurOverlay.addEventListener("click", function () {
-      blurOverlay.classList.add("hidden");
-    });
-
-    // close_offcanvas.addEventListener('click',function(){
-    //     blurOverlay.classList.add("hidden");
-    //     blurOverlay.classList.toggle("hidden");
-    // })
+  // left side menu code 
+  menu_button_left_shopping_cart.addEventListener('click' , function(){
+    offcanvas_of_left_menu.style.visibility = 'visible';
+    blurOverlay.classList.toggle('hidden')
   });
+
+
+  close_btn_of_left_menu_inside.addEventListener('click' , function(){
+    offcanvas_of_left_menu.style.visibility = 'hidden';
+    blurOverlay.classList.toggle('hidden')
+  });
+
+  
+  blurOverlay.addEventListener('click', function () {
+    offcanvas_of_left_menu.style.visibility = 'hidden';
+    blurOverlay.classList.remove('hidden')
+  });
+
+
+  // right side menu code 
+  toggle_for_dropdown.addEventListener('click', function () {
+    dropdown_of_shop.classList.toggle('hidden')
+  });
+
+
+  menu_button_right_bars_3.addEventListener('click', function () {
+    offcanvas_of_right_menu.style.visibility = 'visible';
+  });
+
+  close_btn_of_right_menu_inside.addEventListener('click', function () {
+    offcanvas_of_right_menu.style.visibility = 'hidden';
+    blurOverlay.classList.toggle('hidden')
+  });
+
+  blurOverlay.addEventListener('click', function () {
+    offcanvas_of_right_menu.style.visibility = 'hidden';
+    blurOverlay.classList.remove('hidden')
+  });
+
+  menu_button_right_bars_3.addEventListener("click", function () {
+    blurOverlay.classList.toggle("hidden"); // Show/Hide the overlay
+  });
+
+  // Optional: Click outside the menu to remove the blur
+  blurOverlay.addEventListener("click", function () {
+    blurOverlay.classList.toggle("hidden");
+    outside_click_close_offcanvas.classList.toggle('hidden')
+  });
+
+});
